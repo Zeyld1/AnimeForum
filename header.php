f4786e627f666a759128c1277bcdd145179a170d
dimav <dimav@DMYTRO-BREBAR>
2026-09-07 11:41:25 +0200

    Header edit

---
+++ header.php
@@ -0,0 +1,9 @@
+<header>
+    <h1>Anime Forum</h1>
+
+    <nav>
+        <a href="index.php">Forside</a>
+        <a href="forum.php">Forum</a>
+        <a href="login.php">Login</a>
+    </nav>
+</header>
