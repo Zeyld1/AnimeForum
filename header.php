043acca2eb06efaa64f420480f2dba0a67ab8047
dimav <dimav@DMYTRO-BREBAR>
2026-09-08 09:50:50 +0200

    Header tilføjelse

--- header.php
+++ header.php
@@ -1,9 +1,12 @@
-<header>
-    <h1>Anime Forum</h1>
+<header class="top-header">
+    <div class="logo">
+        <h1>AnimeForum</h1>
+    </div>
 
     <nav>
         <a href="index.php">Forside</a>
         <a href="forum.php">Forum</a>
         <a href="login.php">Login</a>
+        <a href="signup.php">Sign up</a>
     </nav>
 </header>
