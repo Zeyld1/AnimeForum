0ef767a8d1f30d41b07fdf9423c68bac1ded2379
dimav <dimav@DMYTRO-BREBAR>
2026-09-08 09:46:42 +0200

    Database change

--- database.php
+++ database.php
@@ -6,6 +6,19 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+
+$host = "mysql111.unoeuro.com";
+$username = "onepieceisreal_dk";
+$password = "anxGhbrmFB4eRzkc369D";
+$database = "onepieceisreal_dk_db";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Forbindelsen fejlede: " . $conn->connect_error);
+}
+
+?>
 </body>
 </html>
