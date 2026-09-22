590f81896e81f5d726211a0003108f00807dcb6c
dimav <dimav@DMYTRO-BREBAR>
2026-09-22 09:56:49 +0200

    new commit

--- header.php
+++ header.php
@@ -1,9 +1,8 @@
 <header class="top-header">
     
-        <h1>AniMecca</h1>
+        <h1><a href="index.php">AniMecca </a></h1>
 
     <nav class ="menu-tekst">
-        <a href="index.php">Forside</a>
         <a href="forum.php">Forum</a>
         <a href="login.php">Login</a>
         <a href="signup.php">Opret Bruger</a>
